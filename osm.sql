75579b5a1c656b26b6af769b545067e1da79630f
olafurr <olafurlax@gmail.com>
2015-09-05 22:48:55 +0000

    fixed wrong order of coordinates

--- osm.sql
+++ osm.sql
@@ -1,6 +1,7 @@
-DROP FUNCTION find_nearest_waterways(lat double precision, lon double precision, inset numeric);
+
 DROP FUNCTION find_nearest_lakes(lat double precision, lon double precision, inset numeric);
 
+DROP FUNCTION find_nearest_waterways(lat double precision, lon double precision, inset numeric);
 CREATE OR REPLACE FUNCTION find_nearest_waterways(lat double precision, lon double precision, inset numeric default 0.1) 
 RETURNS TABLE (
 	id bigint,
@@ -23,14 +24,15 @@ SELECT DISTINCT
     round((ST_Distance_Sphere(ST_ClosestPoint(way, ST_Centroid(way)), ST_SetSRID(ST_MakePoint(lon, lat), 4326)) / 1000*1.0)::numeric, 2) AS distance
 FROM
   (SELECT way,
-  		  ST_DumpPoints(way) AS dp,
+          ST_DumpPoints(way) AS dp,
           name,
           osm_id
    FROM planet_osm_line
-   WHERE (way && ST_MakeEnvelope(lon - inset, lat - inset, lon + inset, lat + inset, 4326)) AND waterway='river' AND name IS NOT NULL) AS blertz
-   ORDER BY distance ASC;
+   WHERE (way && ST_MakeEnvelope(lon - inset, lat - inset, lon + inset, lat + inset, 4326)) AND waterway='river') AS blert
+   ORDER BY (dp).path;
  END;
  $$ LANGUAGE plpgsql;
+
 
 
 
